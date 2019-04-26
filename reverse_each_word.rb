c1b198bff7b29ab261fbe3b0b2f541bf3623cf2b
Anthony Rurup <tcrurup@gmail.com>
2019-04-26 05:25:35 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,6 @@
 def reverse_each_word(sentence)
-  reversed_sentence = []
-  sentence.split(' ').each do |word|
-    reversed_sentence << word.reverse
+  sentence.split(' ').collect do |word|
+    word.reverse
   end
-  reversed_sentence.join(" ")
+  sentence.join(" ")
 end
